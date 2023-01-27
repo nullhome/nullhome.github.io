90fdfe04061e8e9929990cff54de78bd7c17cdd9
NullHome <gintonic13.6.i@gmail.com>
2023-01-27 10:43:18 +0700

    .

---
+++ asset/js/MusicLibrary.js
@@ -0,0 +1,91 @@
+﻿
+
+/*Onload set style */
+function setStyleForHiddenElement() {
+    document.getElementById("js-menu-sidebar").style.visibility = "hidden";
+    document.getElementById("js-menu-sidebar").style.opacity = 0;
+    document.getElementById("js-queue-sidebar").style.visibility = "hidden";
+    document.getElementById("js-queue-sidebar").style.opacity = 0;
+
+    document.getElementById("js-expand-artist").style.maxHeight = "0px";
+    document.getElementById("js-expand-album").style.maxHeight = "0px";
+    document.getElementById("js-expand-tag").style.maxHeight = "0px";
+    document.getElementById("js-expand-category").style.maxHeight = "0px";
+}
+/*Sidebar toggle */
+function menuSideBarToggle() {
+    var x = document.getElementById("js-menu-sidebar");
+    if (x.style.visibility === "hidden") {
+        x.style.visibility = "visible";
+        x.style.opacity = 1;
+    } else {
+        x.style.visibility = "hidden";
+        x.style.opacity = 0;
+    }
+}
+
+/*Sidebar toggle */
+function randomSideBarToggle() {
+    var x = document.getElementById("js-random-sidebar");
+    if (x.style.visibility === "hidden") {
+        x.style.visibility = "visible";
+        x.style.opacity = 1;
+    } else {
+        x.style.visibility = "hidden";
+        x.style.opacity = 0;
+    }
+}
+
+function queueSideBarToggle() {
+    var x = document.getElementById("js-queue-sidebar");
+    if (x.style.visibility === "hidden") {
+        x.style.visibility = "visible";
+        x.style.opacity = 1;
+    } else {
+        x.style.visibility = "hidden";
+        x.style.opacity = 0;
+    }
+}
+/*Expand T-A-A*/
+function expandArtist() {
+    var x = document.getElementById("js-expand-artist");
+    if (x.style.maxHeight === "0px") {
+        x.style.maxHeight = "6000px";
+        document.getElementById("js-btn-expand-artist").style.transform = "rotate(180deg)";
+    } else {
+        x.style.maxHeight = "0px";
+        document.getElementById("js-btn-expand-artist").style.transform = "rotate(0deg)";
+    }
+}
+
+function expandAlbum() {
+    var x = document.getElementById("js-expand-album");
+    if (x.style.maxHeight === "0px") {
+        x.style.maxHeight = "6000px";
+        document.getElementById("js-btn-expand-album").style.transform = "rotate(180deg)";
+    } else {
+        x.style.maxHeight = "0px";
+        document.getElementById("js-btn-expand-album").style.transform = "rotate(0deg)";
+    }
+}
+
+function expandTag() {
+    var x = document.getElementById("js-expand-tag");
+    if (x.style.maxHeight === "0px") {
+        x.style.maxHeight = "6000px";
+        document.getElementById("js-btn-expand-tag").style.transform = "rotate(180deg)";
+    } else {
+        x.style.maxHeight = "0px";
+        document.getElementById("js-btn-expand-tag").style.transform = "rotate(0deg)";
+    }
+}
+function expandCategory() {
+    var x = document.getElementById("js-expand-category");
+    if (x.style.maxHeight === "0px") {
+        x.style.maxHeight = "6000px";
+        document.getElementById("js-btn-expand-category").style.transform = "rotate(180deg)";
+    } else {
+        x.style.maxHeight = "0px";
+        document.getElementById("js-btn-expand-category").style.transform = "rotate(0deg)";
+    }
+}
---
+++ asset/js/clock.js
@@ -0,0 +1,28 @@
+﻿function currentTime() {
+    let date = new Date();
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+
+    let dd = date.getDate();
+    let yyyy = date.getFullYear();
+    let day = weekday[date.getDay()];
+    let MM = month[date.getMonth()];
+
+    dd = (dd < 10) ? "0" + dd : dd;
+
+    let calendar = day + ", " + dd + " " + MM + " " + yyyy;
+
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+
+    let time = hh + ":" + mm;
+
+    document.getElementsByClassName("date-time__clock")[0].innerText = time;
+
+    document.getElementsByClassName("date-time__calendar")[0].innerText = calendar;
+    let t = setTimeout(function () { currentTime() }, 15000);
+}
+currentTime();
